70638b51a093468329936b496f4dee26ea1e9bf6
phuoctaiismee <113773744+phuoctaiismee@users.noreply.github.com>
2024-11-23 17:07:50 +0700

    fix: add slice zone in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-    return <main className="flex flex-col mx-auto items-center justify-center">Hello</main>;
+import { Metadata } from "next";
+import { SliceZone } from "@prismicio/react";
+
+import { createClient } from "@/prismicio";
+import { components } from "@/slices";
+
+export default async function Page() {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
+
+  return <SliceZone slices={page.data.slices} components={components} />;
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+  };
 }
