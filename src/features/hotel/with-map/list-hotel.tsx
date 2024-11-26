100043323ec4f5a5a6b7bbdbabc409803d9be549
tran hien <tronghientran18@gmail.com>
2024-11-26 17:53:16 +0700

    [feat] - merge

--- src/features/hotel/with-map/list-hotel.tsx
+++ src/features/hotel/with-map/list-hotel.tsx
@@ -4,7 +4,7 @@ import HotelCard from '../cards/hotel-card';
 export const ListHotel = () => {
    return (
       <div className="grid gap-6 md:gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
-         {mockTours?.map((property, index) => <HotelCard {...property} key={index} />)}
+         {mockTours?.map((property, index) => <HotelCard {...property} displayType='list' key={index} />)}
       </div>
    );
 };
--- src/slices/HowItWork/index.tsx
+++ src/slices/HowItWork/index.tsx
@@ -10,83 +10,79 @@ import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from '@prismic
  */
 export type HowItWorkProps = SliceComponentProps<Content.HowItWorkSlice>;
 const components: JSXMapSerializer = {
-    heading2: ({ children }) => <Heading>{children}</Heading>,
-    paragraph: ({ children }) => <Paragraph>{children}</Paragraph>,
-}
+   heading2: ({ children }) => <Heading>{children}</Heading>,
+   paragraph: ({ children }) => <Paragraph>{children}</Paragraph>,
+};
 /**
  * Component for "HowItWork" Slices.
  */
 const HowItWork = ({ slice }: HowItWorkProps): JSX.Element => {
-    return (
-        <section
-            data-slice-type={slice.slice_type}
-            data-slice-variation={slice.variation}
-            className="py-16"
-        >
+   return (
+      <section
+         data-slice-type={slice.slice_type}
+         data-slice-variation={slice.variation}
+         className="py-16"
+      >
+         {slice.variation == 'default' && (
             <Bounded>
-                <div className="flex flex-col justify-center items-center gap-10">
-                    <div className="nc-Section-Heading relative flex flex-col sm:flex-row sm:items-end justify-between mb-12 lg:mb-16 text-neutral-900 dark:text-neutral-50">
-                        <div className="text-center w-full max-w-2xl mx-auto">
-                            <PrismicRichText
-                                field={slice.primary.heading}
-                                components={components}
-                            />
-                            <PrismicRichText
-                                field={slice.primary.body}
-                                components={components}
-                            />
+               <div className="flex flex-col justify-center items-center gap-10">
+                  <div className="nc-Section-Heading relative flex flex-col sm:flex-row sm:items-end justify-between mb-12 lg:mb-16 text-neutral-900 dark:text-neutral-50">
+                     <div className="text-center w-full max-w-2xl mx-auto">
+                        <PrismicRichText field={slice.primary.heading} components={components} />
+                        <PrismicRichText field={slice.primary.body} components={components} />
+                     </div>
+                  </div>
+                  <div className="relative grid md:grid-cols-3 gap-20 mt-10">
+                     <img
+                        src="/dash.svg"
+                        alt="dash"
+                        className="hidden md:block absolute inset-x-0 top-10"
+                     />
+                     {slice.primary.howitworks.map((item, index) => (
+                        <div
+                           key={index}
+                           className="relative flex flex-col gap-5 items-center max-w-xs mx-auto"
+                        >
+                           <div className="max-w-[200px] mx-auto">
+                              <PrismicNextImage
+                                 field={item.image_light}
+                                 alt=""
+                                 className="object-cover w-full h-full block dark:hidden"
+                              />
+                              <PrismicNextImage
+                                 field={item.image_dark}
+                                 alt=""
+                                 className="object-cover w-full h-full hidden dark:block"
+                              />
+                           </div>
+                           <div className="text-center mt-auto">
+                              <PrismicRichText
+                                 field={item.title}
+                                 components={{
+                                    heading3: ({ children }) => (
+                                       <h3 className="text-xl font-semibold">{children}</h3>
+                                    ),
+                                 }}
+                              />
+                              <PrismicRichText
+                                 field={item.description}
+                                 components={{
+                                    paragraph: ({ children }) => (
+                                       <p className="block mt-5 text-neutral-500 dark:text-neutral-400">
+                                          {children}
+                                       </p>
+                                    ),
+                                 }}
+                              />
+                           </div>
                         </div>
-                    </div>
-                    <div className="relative grid md:grid-cols-3 gap-20 mt-10">
-                        <img
-                            src="/dash.svg"
-                            alt="dash"
-                            className="hidden md:block absolute inset-x-0 top-10"
-                        />
-                        {slice.primary.howitworks.map((item, index) => (
-                            <div
-                                key={index}
-                                className="relative flex flex-col gap-5 items-center max-w-xs mx-auto"
-                            >
-                                <div className="max-w-[200px] mx-auto">
-                                    <PrismicNextImage
-                                        field={item.image_light}
-                                        alt=""
-                                        className="object-cover w-full h-full block dark:hidden"
-                                    />
-                                    <PrismicNextImage
-                                        field={item.image_dark}
-                                        alt=""
-                                        className="object-cover w-full h-full hidden dark:block"
-                                    />
-                                </div>
-                                <div className="text-center mt-auto">
-                                    <PrismicRichText
-                                        field={item.title}
-                                        components={{
-                                            heading3: ({ children }) => (
-                                                <h3 className="text-xl font-semibold">{children}</h3>
-                                            ),
-                                        }}
-                                    />
-                                    <PrismicRichText
-                                        field={item.description}
-                                        components={{
-                                            paragraph: ({ children }) => (
-                                                <p className="block mt-5 text-neutral-500 dark:text-neutral-400">
-                                                    {children}
-                                                </p>
-                                            ),
-                                        }}
-                                    />
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
+                     ))}
+                  </div>
+               </div>
             </Bounded>
-        </section>
-    );
+         )}
+      </section>
+   );
 };
 
 export default HowItWork;
